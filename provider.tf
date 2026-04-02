4f9a38537c1c7d2fef7213e296443726be4e8027
Konstantin Chigridov <werest54@rambler.ru>
2026-04-02 18:00:40 +0300

    Init 2

--- provider.tf
+++ provider.tf
@@ -8,17 +8,13 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "tfstate-bucket-student-bucket-diploma"
-    region = "ru-central1"
-    key    = "main/terraform.tfstate"
-    endpoints = {
-      s3 = "https://storage.yandexcloud.net"
-    }
+    bucket   = "tfstate-bucket-student-bucket-diploma"
+    region   = "ru-central1"
+    key      = "main/terraform.tfstate"
+    endpoint = "https://storage.yandexcloud.net"
 
     skip_region_validation      = true
     skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
   }
 }
 
