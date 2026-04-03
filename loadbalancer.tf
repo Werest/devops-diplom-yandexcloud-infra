b73ba1369b8fed67d1eb808f1c63232bbe73f20a
Konstantin Chigridov <werest54@rambler.ru>
2026-04-03 13:32:58 +0300

    LB + SG add port

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,30 +1,3 @@
-# Группа безопасности для балансировщика
-resource "yandex_vpc_security_group" "lb-sg" {
-  name        = "lb-sg"
-
-  ingress {
-    protocol       = "TCP"
-    description    = "HTTP"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 80
-  }
-
-  ingress {
-    protocol       = "TCP"
-    description    = "Порт 3100"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 3100
-  }
-
-  egress {
-    protocol       = "ANY"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    from_port      = 0
-    to_port        = 65535
-  }
-  network_id = yandex_vpc_network.k8s_net.id
-}
-
 resource "yandex_lb_target_group" "lb-balancer-group" {
   name       = "lb-balancer-group"
   depends_on = [yandex_compute_instance.master]
@@ -41,8 +14,6 @@ resource "yandex_lb_target_group" "lb-balancer-group" {
 # LB Grafana
 resource "yandex_lb_network_load_balancer" "lb-grafana" {
   name = "lb-grafana"
-
-  security_group_ids = [yandex_vpc_security_group.lb-sg.id]
 
   listener {
     name        = "grafana-listener"
@@ -73,8 +44,6 @@ resource "yandex_lb_network_load_balancer" "lb-grafana" {
 # LB WEBAPP
 resource "yandex_lb_network_load_balancer" "lb-webapp" {
   name = "lb-webapp"
-
-  security_group_ids = [yandex_vpc_security_group.lb-sg.id]
 
   listener {
     name        = "webapp-listener"
